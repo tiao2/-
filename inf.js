1e3b4e2ba3cc73dc7b0c74b1e2f59fb181e887f5
Tiaotiao <65841827@qq.com>
2024-10-17 20:33:06 +0800

    Create inf.js

---
+++ inf.js
@@ -0,0 +1,8 @@
+function inf(n){
+return [{
+  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-63e09ad2bf4e44a71a271903?",
+  "time": "2023-02-06 06:14:42.000",
+  "link-number": 5,
+  "words": 1968
+}][n-1];
+}
