ddc6bcb51c44c1e0fe04298181a0e03b5f7c13ce
Tiaotiao <65841827@qq.com>
2024-10-17 20:34:07 +0800

    Update inf.js

--- inf.js
+++ inf.js
@@ -4,5 +4,10 @@ return [{
   "time": "2023-02-06 06:14:42.000",
   "link-number": 5,
   "words": 1968
+},{
+  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-63ef9824bf4e44a71a2965e4?",
+  "time": "2023-02-17 15:07:16.000",
+  "link-number": 0,
+  "words": 118
 }][n-1];
 }
