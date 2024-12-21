610eff4aec4f6c3e7d54aa8df4ee4300e2c7a027
Tiaotiao <65841827@qq.com>
2024-12-21 14:11:37 +0800

    Update inf.js

--- inf.js
+++ inf.js
@@ -1,21 +1,21 @@
 function inf(n){
 return [{
-  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-63e09ad2bf4e44a71a271903?",
+  "link": "physics://chinese/discussion/63e09ad2bf4e44a71a271903",
   "time": "2023-02-06 06:14:42.000",
   "link-number": 10,
   "words": 1968
 },{
-  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-63ef9824bf4e44a71a2965e4?",
+  "link": "physics://chinese/discussion/63ef9824bf4e44a71a2965e4",
   "time": "2023-02-17 15:07:16.000",
   "link-number": 0,
   "words": 118
 },{
-  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-6441e70f897af533de30a874?",
+  "link": "physics://chinese/discussion/6441e70f897af533de30a874",
   "time": "2023-04-21 01:29:51.000",
   "link-number": 0,
   "words": 1260
 },{
-  "link": "https://dl.turtlesim.com/plc/?chinese-discussion-66b5ec1b0c0a3e021bf99a44?",
+  "link": "physics://chinese/discussion/66b5ec1b0c0a3e021bf99a44",
   "time": "2024-08-09 10:14:51.000",
   "link-number": 5,
   "words": 1862
